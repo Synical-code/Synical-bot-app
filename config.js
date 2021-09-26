ffb239a61432b86831e5817817bcecf0ec438796
Matic F <klarimadu@gmail.com>
2021-09-26 16:28:38 +0200

    initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    app: {
+        px: '-',
+        token: 'ODkxMzY1MDQ0NzYyMzQ1NTIy.YU9SYw._7owqH_AreXQ2Aa7Yseg3x7FeKo',
+        playing: 'Watching over Synical World',
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: 'Bots',
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
+        },
+        maxVol: 100,
+        discordPlayer: {}
+    }
+};
